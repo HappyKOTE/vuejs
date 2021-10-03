514f08d29c22e78634852f116d5cc5e2dd5565e7
HappyKOTE <vpavlov.moscow@gmail.com>
2021-10-03 23:27:40 +0300

    fix server bugs

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/dist'));
 
 const port = process.env.PORT || 8080;
 app.listen(port);
